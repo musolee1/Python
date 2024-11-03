72f61d19caa4a646ce7eac6506278f3da227ab6e
Mustafa <mustafabayramoglu29@gmail.com>
2024-11-04 02:43:43 +0300

    I started to functions on python

---
+++ Functions/functions.py
@@ -0,0 +1,15 @@
+# def greeting(): // Function definition
+#     print("Hello, World!")
+
+# greeting()
+
+# for i in range(5):
+#     greeting()
+
+# a = 10
+# b = 20
+
+# def plus():
+#     print(a+b)
+
+# plus()
---
+++ Functions/return.py
@@ -0,0 +1,30 @@
+a = 20
+b = 10
+
+def plus():
+    return f'Sum of a and b is: {a+b}'
+
+print(plus())
+
+def now():
+    import datetime
+    return datetime.datetime.now().year
+
+def hour():
+    import datetime
+    return datetime.datetime.now().hour
+
+def ageCalculation():
+    return now() - 1995
+
+print(ageCalculation())
+
+def greeting():
+    if (hour()<12):
+        return "Good Morning"
+    elif (hour()>=12 and hour()<18):
+        return "Good Afternoon"
+    else:  
+        return "Good Night"
+    
+print(greeting())
