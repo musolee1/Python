53af10fcb0e4c7be1f5cfc69a9162e07febddf94
Mustafa <mustafabayramoglu29@gmail.com>
2024-11-08 23:42:29 +0300

    math modules

---
+++ Modules/demo-module/math.py
@@ -0,0 +1,25 @@
+# import math as islem
+
+# value = dir(math)
+
+# value = help(math)
+
+# value = help(math.factorial)
+
+# value = math.sqrt(49)
+
+# value = math.factorial(4)
+
+# # value = math.floor(5.9)
+
+# value = math.ceil(5.9)
+
+# value = islem.factorial(5)
+
+from math import *
+
+value = factorial(5)
+
+value = sqrt(25)
+
+print(value)
