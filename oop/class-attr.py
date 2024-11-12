6ce846b55eb285e9c197b860104616448de51943
Mustafa <mustafabayramoglu29@gmail.com>
2024-11-12 15:18:52 +0300

    Quiz App tamamlandı.

--- oop/class-attr.py
+++ oop/class-attr.py
@@ -1,14 +1,27 @@
 class User:
+    active_users = 0
+
     def __init__(self, first, last, age):
         self.first = first
         self.last = last
         self.age = age
+        User.active_users += 1
     
     def full_name(self):
         return f"{self.first} {self.last}"
+    
+    def logout(self):
+        User.active_users -= 1
+        return f"{self.full_name()} uygulamadan çıkış yaptı."
+
+print(User.active_users)
 
 userA = User("Mustafa", "Bayramoğlu", 23)
 userB = User("İlayda", "Bulut", 23)
+userC = User("Alp", "Koç", 4)
 
-print(userA.full_name())
-print(userB.full_name())
+print(User.active_users)
+
+print(userC.logout())
+
+print(User.active_users)
